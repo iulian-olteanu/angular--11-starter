9d561e685c60d31e2639e4abc280d252e1d4f755
iulianolteanu <iulian.olteanu@rodevel.net>
2021-01-01 14:36:22 +0200

    .

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -8,6 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
@@ -21,6 +23,8 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatSlideToggleModule,
+    MatTooltipModule,
   ],
   exports: [
     MatInputModule,
@@ -31,6 +35,8 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatSlideToggleModule,
+    MatTooltipModule,
   ],
 })
 export class AppMaterialModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ColorSchemeService } from './services/color-scheme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-dream-app';
+
+  /**
+   *
+   */
+  constructor(private colorSchemeService: ColorSchemeService) {
+    // Load Color Scheme
+    this.colorSchemeService.load();
+  }
 }
